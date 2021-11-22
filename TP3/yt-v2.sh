7a52583aa07762eb2420f75d85dcdc4200cf08f2
leo-grandmorcel <92717649+leo-grandmorcel@users.noreply.github.com>
2021-11-22 09:53:42 +0100

    Verif SellCheck

--- TP3/yt-v2.sh
+++ TP3/yt-v2.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 while true; do
     if [[ -d "/srv/yt/downloads" && -d "/var/log/yt/" && -s "/srv/yt/url" ]]; then 
-        cat /srv/yt/url | while read line; do
-	        youtube-dl -e $line &> /dev/null
-            if [[ $? == 0 ]]; then
-                title=$(youtube-dl -e $line) 
+        while read -r line < /srv/yt/url; do
+            if youtube-dl -e "$line" &> /dev/null; then
+                title=$(youtube-dl -e "$line") 
                 mkdir "/srv/yt/downloads/$title"
-                youtube-dl -o "/srv/yt/downloads/$title/$title.mp4" --format mp4 $line > /dev/null
-                youtube-dl --get-description $line > "/srv/yt/downloads/$title/description"
+                youtube-dl -o "/srv/yt/downloads/$title/$title.mp4" --format mp4 "$line" > /dev/null
+                youtube-dl --get-description "$line" > "/srv/yt/downloads/$title/description"
                 echo "Video $line was downloaded"
                 echo "File path : /srv/yt/downloads/$title/$title.mp4"
                 echo "[$(date "+%D %T")]"" Video $line was downloaded. File path : ""/srv/yt/downloads/$title/$title.mp4""'" >> "/var/log/yt/download.log"
@@ -20,4 +19,3 @@ while true; do
     fi
     sleep 5s
 done
-
