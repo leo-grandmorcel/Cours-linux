be19ac9a3133ea9827cab9891d6e5005df68be1b
LGRAND-MORCEL <leo.grandmorcel@ynov.com>
2021-11-22 16:18:34 +0100

    Modification pour bonus

--- TP3/yt.sh
+++ TP3/yt.sh
@@ -1,12 +1,59 @@
-if [[ -d "/srv/yt/downloads" && -d "/var/log/yt/" ]]; then 
-	title=$(youtube-dl -e $1) 
-	mkdir "/srv/yt/downloads/$title"
-	youtube-dl -o "/srv/yt/downloads/$title/$title.mp4" --format mp4 $1 > /dev/null
-	youtube-dl --get-description $1 > "/srv/yt/downloads/$title/description"
-	echo "Video $1 was downloaded"
-	echo "File path : /srv/yt/downloads/$title/$title.mp4"
-	echo "[$(date "+%D %T")]"" Video $1 was downloaded. File path : ""/srv/yt/downloads/$title/$title.mp4""'" >> "/var/log/yt/download.log"
+#!/bin/bash
+usage() {
+        echo "Usage: yt.sh [options] <url>"
+        echo " -q <quality> "
+        echo " -o <directory> path file"
+        echo " -h to show this menu "
+        exit
+}
+
+if (! (command -v python) && ! (command -v youtube-dl)) &> /dev/null; then
+        echo "Get youtube-dl and python"
+        exit
+fi
+if [[ -d "/srv/yt/downloads" && -d "/var/log/yt/" ]]; then
+        path="/srv/yt/downloads"
+        format="mp4"
+        for i in "$@"; do
+                if [[ "$i" == "-q" ]]; then
+                        path=true
+                elif [[ "$i" == "-o" ]]; then
+                        format=true
+                elif [[ "$i" == "-h" ]]; then
+                        echo "Usage : yt.sh [options] url"
+                        echo " Options : -q <quality>   -o <directory> File path"
+                        exit
+                elif [[ $format == true ]]; then
+                        format="$i"
+                elif [[ $path == true ]]; then
+                        path="$i"
+                fi
+                lien="$i"
+        done
+        if [[ $lien =~ https://www.youtube.com/ ]]; then
+                if youtube-dl -e "$lien" &> /dev/null; then
+                        title=$(youtube-dl -e "$lien")
+                        mkdir "$path/$title" &> /dev/null
+                        if (cd "$path/$title" && youtube-dl -f "$format/mp4" "$lien" &>/dev/null); then
+                                youtube-dl --get-description "$lien" > "$path/$title/description"
+                                echo "Video $lien was downloaded in $format format."
+                                echo "File path : $path/$title/$title.mp4"
+                                echo "[$(date "+%D %T")]"" Video $lien was downloaded. File path : ""$path/$title/$title.mp4""'" >> "/var/log/yt/download.log"
+                        else
+                                echo "Video download failed"
+				echo "[$(date "+%D %T")]"" Video $lien failed to download.""'" >> "/var/log/yt/download.log"
+                                usage
+                        fi
+                else
+			echo "[$(date "+%D %T")]"" Link $lien is not working.""'" >> "/var/log/yt/download.log"
+                        echo "Link not working"
+                        usage
+                fi
+        else
+		echo "[$(date "+%D %T")]"" Wrong URL : $lien""'" >> "/var/log/yt/download.log"
+                echo "wrong URL"
+                usage
+        fi
 else
-	echo "Pas trouvé"
-	exit
+        echo "Dossier manquant"
 fi
